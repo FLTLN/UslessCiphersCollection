06911d051431ff1e52466b68f1100b4b5dc7a7f5
Maksim Zakharov <mttpr28@gmail.com>
2022-03-28 20:36:18 +0300

    [TEST][NEW] Add demo test to configure GitHub actions

---
+++ tests/DemoTest.cpp
@@ -0,0 +1,8 @@
+#include "tests.h"
+#include <iostream>
+
+bool t_demo_test()
+{
+   std::cout << "IMA DEMO TEST TO CONFIGURE ACTIONS\n";
+   return true;
+}
---
+++ tests/routine/tests.cpp
@@ -0,0 +1,46 @@
+#define EXECUTE_TEST(test)                                                                                                                                           \
+   std::cout << "───────────────────────────────────────────────\n";                                                                                                 \
+   std::cout << "Test: " << #test << "\n";                                                                                                                           \
+   std::cout << "───────────────────────────────────────────────\n";                                                                                                 \
+   std::cout << "Test output:\n";                                                                                                                                    \
+   all++;                                                                                                                                                            \
+   std::cout << "───────────────────────────────────────────────\n";                                                                                                 \
+   if (test())                                                                                                                                                       \
+   {                                                                                                                                                                 \
+      std::cout << "───────────────────────────────────────────────\n" \
+                << "Test passed\n";                                                                                                                                  \
+      passed++;                                                                                                                                                      \
+   }                                                                                                                                                                 \
+   else                                                                                                                                                              \
+   {                                                                                                                                                                 \
+      std::cout << "Test failed\n";                                                                                                                                  \
+      isFailed = true;                                                                                                                                               \
+      failed++;                                                                                                                                                      \
+   }                                                                                                                                                                 \
+   std::cout << "───────────────────────────────────────────────\n\n";
+
+#include <iostream>
+#include "tests.h"
+
+int main()
+{
+   bool isFailed = false;
+   int passed = 0;
+   int failed = 0;
+   int all = 0;
+
+   // key expansion
+   EXECUTE_TEST(t_demo_test)
+
+   std::cout << "Testing done\n";
+   std::cout << "───────────────────────────────────────────────\n";
+   std::cout << "Tests executed : " << all << "\n";
+   std::cout << "Tests passed   : " << passed << "\n";
+   std::cout << "Tests failed   : " << failed << "\n";
+   std::cout << "───────────────────────────────────────────────\n";
+
+   if (isFailed)
+      return 1;
+   else
+      return 0;
+}
